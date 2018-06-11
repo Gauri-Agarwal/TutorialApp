6dcb65385d6f739a4f0f8afc457d63b2d0c95cb6
Gauri <gauria.andrew.cmu.edu>
2018-06-11 15:00:54 -0400

    converted the app into a git repo

---
+++ Basic/src/util/connectors.js
@@ -0,0 +1,9 @@
+
+import { Connect, SimpleSigner } from 'uport-connect'
+
+export const uport = new Connect('bbBasicApp', {
+  clientId: '2oujSyFB5ua4WQYdndzq31xc22kBNRZNxNZ',
+  network: 'rinkeby',
+  signer: SimpleSigner('4bca4b663db8e626ad1ae38f430fbe9c3179399cd72feb81db477ff6383950e7')
+})
+
